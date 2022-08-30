2a49b7ef3a7a64a647b8a91cef72f8f50dcb0421
ZhdanovO <oleg.zhdanov88@gmail.com>
2022-08-30 15:12:22 +0300

    create WeatherActivity.kt and WeatherPresenter.kt and activity_weather.xml

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.weatherapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val texViewHello = findViewById<TextView>(R.id.tvHello)
-        texViewHello.text ="<p>Привет!</p> мир!" +
-                "changes just fore test github connection"
+        texViewHello.text = "Hello from code!"
+
+        val btnWeather = findViewById<Button>(R.id.btnWeather)
+        btnWeather.setOnClickListener {
+            Intent(this, WeatherActivity::class.java).also { startActivity(it) }
+        }
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/WeatherPresenter.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp
+
+class WeatherPresenter {
+
+    fun getWeather(temperature: String) : String {
+        return temperature
+    }
+}
