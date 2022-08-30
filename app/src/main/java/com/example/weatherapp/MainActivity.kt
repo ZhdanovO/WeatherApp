69f8f4857e94f63ae082abf4a0ee1d435fc31a17
ZhdanovO <oleg.zhdanov88@gmail.com>
2022-08-30 13:40:33 +0300

    - Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val texViewHello = findViewById<TextView>(R.id.tvHello)
+        texViewHello.text ="<p>Привет!</p> мир!"
+    }
+}
